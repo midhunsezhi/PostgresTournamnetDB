df7abe7b89101ddcb23281717ce6662f31e7298b
Midhun Dandamudi <midhunsezhi@gmail.com>
2017-04-22 12:29:47 -0700

    define schema for tournament

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -1,4 +1,9 @@
 -- Table definitions for the tournament project.
 
 
+CREATE TABLE players (id SERIAL PRIMARY KEY,
+                      name TEXT NOT NULL);
 
+CREATE TABLE matches (id SERIAL PRIMARY KEY,
+                      winner REFERENCES players(id) NOT NULL,
+                      loser REFERENCES players(id) NOT NULL)
